1c8131b75a163d6d5676ac1e0c12b4a8797056ee
Antonio Fulgêncio <antoniofulg@gmail.com>
2020-11-07 15:11:41 -0300

    feature: setup vuetify

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import pt from 'vuetify/es5/locale/pt'
+
+Vue.use(Vuetify)
+
+export default new Vuetify({
+  theme: {
+    options: {
+      customProperties: true
+    },
+    themes: {
+      light: {
+        primary: '#09A5DF',
+        secondary: '#00223C',
+        accent: '#0066B3',
+        error: '#FF5252',
+        info: '#2196F3',
+        success: '#32CF7F',
+        warning: '#FFC107'
+      }
+    }
+  },
+  lang: {
+    locales: { pt },
+    current: 'pt'
+  },
+  icons: {
+    iconfont: 'fa'
+  }
+})
